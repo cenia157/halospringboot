72c0eed7383f2dcd322803653f4b00919c3922b5
shw <a41748734@gmail.com>
2024-02-14 18:18:39 +0900

    hw

--- src/main/java/com/halo/main/admin/userpage_update/quick_menu/QuickMenuDAO.java
+++ src/main/java/com/halo/main/admin/userpage_update/quick_menu/QuickMenuDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class QuickMenuDAO {
@@ -16,8 +17,11 @@ public class QuickMenuDAO {
 		return qMenuMapper.getListQuickMenu();
 	}
 
-	public int updateQuickMenu(String m_name4,String m_name5,String m_name6,
+	public int updateQuickMenu(@RequestParam("m_name4") String m_name4,
+			@RequestParam("m_name5") String m_name5,
+			@RequestParam("m_name6") String m_name6,
 			 Model model) {
+		System.out.println("DAOvalue3개 : "+ m_name4 +", "+ m_name5 +", "+ m_name6);
 		String[] quickMenuValues = {m_name4, m_name5, m_name6};
 		//셀렉에서 받아온 m_name4,5,6 3번 돌리기
 		for (int i = 0; i < 3; i++) {
--- src/main/java/com/halo/main/admin/userpage_update/quick_menu/QuickMenuMapper.java
+++ src/main/java/com/halo/main/admin/userpage_update/quick_menu/QuickMenuMapper.java
@@ -3,6 +3,7 @@ package com.halo.main.admin.userpage_update.quick_menu;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 @Mapper
@@ -10,10 +11,11 @@ public interface QuickMenuMapper {
 	@Select("select * from banner_tbl, menu_tbl where b_index > 3 and b_m_name = m_name")
 	List<QuickMenuDTO> getListQuickMenu();
 	
-	@Update("update banner_tbl set b_m_name = #{m_name},"
-			+ "b_url=(select m_text from menu_tbl where m_name=#{m_name})"
-			+ "b_m_text = (select m_text from menu_tbl where m_name=#{m_name})"
-			+ "where b_index=#{i}")
-	int updateQuickMenu(String m_name, int i);
+	@Update("UPDATE banner_tbl"
+			+ "SET b_m_name = #{name},"
+			+ "    b_url = (SELECT m_text FROM menu_tbl WHERE m_name = #{name}),"
+			+ "    b_m_text = (SELECT m_text FROM menu_tbl WHERE m_name = #{name})"
+			+ "WHERE b_index = #{i}")
+	int updateQuickMenu(@Param("name") String name, @Param("i") int i);
 
 }
